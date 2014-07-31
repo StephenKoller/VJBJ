18818634888d7e3ffed54dc71896666f8b20a70f
Steve Koller <skoller@pillartechnology.com>
2014-07-31 18:05:03 +0200

    Updated workflow

--- js/blackjack.js
+++ js/blackjack.js
@@ -8,6 +8,7 @@ function Player(name){
 function Game(){
   this.deck = [];
   this.createDeck(this.deck);
+  this.bet = 0;
 }
 
 Game.prototype.createDeck = function(deckArray){
@@ -20,9 +21,14 @@ Game.prototype.createDeck = function(deckArray){
   // 10, ensuring face cards always have a value of 10.
   for(suit in suits){
     for(value in values){
+      if(values[value].substring(0,1) === "A"){
+        var cardVal = 11;
+      } else {
+        var cardVal = Math.min(10, parseInt(value)+1);
+      }
       var tmpCard = {
           "name" : values[value] + suits[suit],
-          "value" : Math.min(10, parseInt(value)+1)
+          "value" : cardVal
         };
       this.deck.push(tmpCard);
     }
@@ -69,16 +75,17 @@ Game.prototype.calculateScore = function(player){
   // Add the value of each card in the injected player's hand
   for(card in player.cards){
     // If the card is an ace, and if the total of all cards in their
-    // hand is under or at 21
-    if(player.cards[card].name.substring(0,1) === "A" && player.totalScore+11 <= 21){
-      // set the ace's value to 11
-      player.cards[card].value = 11;
+    // hand is over 21
+    if(player.cards[card].name.substring(0,1) === "A" && player.totalScore + 11 > 21){
+      // set the ace's value to 1
+      player.cards[card].value = 1;
     }
     player.totalScore += player.cards[card].value;
   }
   return player.totalScore;
 }
 
-Game.prototype.calculateBet = function(){
-  
+Game.prototype.addBet = function(amount){
+  game.bet += amount;
+  player.money -= amount;
 }
