d76daeb63bbb016039e4eee44cce5615975a36b5
Steve Koller <skoller@pillartechnology.com>
2014-07-31 15:54:13 +0200

    Fixed bugs around running out of cards, allowing dealer to bust but saying dealer wins

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,21 +1,29 @@
-function Player(){
-  this.money = 100;
+function Player(name){
+  this.money = 1000;
   this.totalScore = 0;
   this.cards = [];
+  this.name = name;
 }
 
 function Game(){
-  // Set up the card deck
-  var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  var suits = ["S", "H", "C", "D"];
   this.deck = [];
+  this.createDeck(this.deck);
+}
 
-  // The Math.min function here sets everything 10 or greater to just 10,
-  // ensuring face cards always have a value of 10.
+Game.prototype.createDeck = function(deckArray){
+  // Set up the card deck
+  var values = ["A", "2", "3", "4", "5", "6","7",
+                "8", "9", "10", "J", "Q", "K"];
+  var suits = ["S", "H", "C", "D"];
 
+  // The Math.min function here sets everything 10 or greater to just
+  // 10, ensuring face cards always have a value of 10.
   for(suit in suits){
     for(value in values){
-      var tmpCard = { "name" : values[value] + suits[suit], "value" : Math.min(10, parseInt(value)+1) };
+      var tmpCard = {
+          "name" : values[value] + suits[suit],
+          "value" : Math.min(10, parseInt(value)+1)
+        };
       this.deck.push(tmpCard);
     }
   }
@@ -31,7 +39,8 @@ Game.prototype.dealCard = function(player, count){
 }
 
 Game.prototype.shuffleCards = function(){
-  // Found this nice Fisher-Yates shuffle algorithm at http://bost.ocks.org/mike/shuffle/
+  // Found this nice Fisher-Yates shuffle algorithm at
+  // http://bost.ocks.org/mike/shuffle/
   var deck = this.deck;
   var unshuffled = deck.length,
     currentCard,
@@ -59,7 +68,8 @@ Game.prototype.calculateScore = function(player){
 
   // Add the value of each card in the injected player's hand
   for(card in player.cards){
-    // If the card is an ace, and if the total of all cards in their hand is under/at 21
+    // If the card is an ace, and if the total of all cards in their
+    // hand is under or at 21
     if(player.cards[card].name.substring(0,1) === "A" && player.totalScore+11 <= 21){
       // set the ace's value to 11
       player.cards[card].value = 11;
@@ -67,4 +77,8 @@ Game.prototype.calculateScore = function(player){
     player.totalScore += player.cards[card].value;
   }
   return player.totalScore;
+}
+
+Game.prototype.calculateBet = function(){
+  
 }
