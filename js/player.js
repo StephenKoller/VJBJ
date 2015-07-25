e3e9687a46f9d55c1d33abf11b0ff9f8bcf96d1f
Stephen Koller <stephenkoller@gmail.com>
2015-07-25 14:28:29 -0400

    Replaced variable names in for loops with 'i'

--- js/player.js
+++ js/player.js
@@ -20,24 +20,24 @@ Player.prototype.calculateScore = function(){
   this.totalScore = 0;
 
   // Add the value of each card in the player's hand
-  for(var card = 0; card < this.cards.length; card++){
-    this.totalScore += this.cards[card].value;
+  for(var i = 0; i < this.cards.length; i++){
+    this.totalScore += this.cards[i].value;
   }
 
-  // TODO(?): account for possible 3 ace situation
+  // TODO: account for possible 3 ace situation
 
   // if total > 21
   if(this.totalScore > 21){
     var aceCount = 0;
     this.totalScore = 0;
 
-    for(var card = 0; card < this.cards.length; card++){
-      if(this.cards[card].name.substring(0,1) === "A" && aceCount === 0){
+    for(i = 0; i < this.cards.length; i++){
+      if(this.cards[i].name.substring(0,1) === "A" && aceCount === 0){
         // set the ace's value to 1
-        this.cards[card].value = 1;
+        this.cards[i].value = 1;
         aceCount++;
       }
-      this.totalScore += this.cards[card].value;
+      this.totalScore += this.cards[i].value;
     }
   }
 
