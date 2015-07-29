ae1445c26f56b422614cb7b8ea5fa1fa2c888611
Stephen Koller <stephenkoller@gmail.com>
2015-07-28 20:40:10 -0400

    Optimized loops in createDeck function

--- js/game.js
+++ js/game.js
@@ -37,16 +37,15 @@ Game.prototype.createDeck = function(){
 
   // The Math.min function here sets everything 10 or greater to just
   // 10, ensuring face cards always have a value of 10.
-
-  for(var i = 0; i < suits.length; i++){
-    for(var j = 0; j < values.length; j++){
-      if(values[j].substring(0,1) === "A"){
+  for(var i = 0, j = suits.length; i < j; i++){
+    for(var k = 0, l = values.length; k < l; j++){
+      if(values[k].substring(0,1) === "A"){
         cardVal = 11;
       } else {
         cardVal = Math.min(10, parseInt(j)+1);
       }
       var tmpCard = {
-          "name" : values[j] + suits[i],
+          "name" : values[k] + suits[i],
           "value" : cardVal
         };
       deck.push(tmpCard);
