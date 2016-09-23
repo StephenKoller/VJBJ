b00c4580fdbf04f6f705e0ce9045e2e60bbc6c9b
Stephen Koller <stephenkoller@gmail.com>
2016-09-23 10:12:33 -0400

    Add NPM, babel

---
+++ V2/test/spec/blackjackSpec.js
@@ -0,0 +1,45 @@
+describe("Game", function() {
+  var player;
+  var game;
+
+  beforeEach(function() {
+    game = new Game();
+    player1 = new Player();
+  });
+
+  it("should have a player1 with a score of 100", function() {
+    expect(player1.money).toEqual(100);
+  });
+
+  describe("when dealing cards it", function(){
+    it("should deal each card only once if dealing a full deck", function(){
+      game.dealCard(player1,52);
+
+      expect(player1.cards.length).toEqual(52);
+
+      // expect(player1.cards.indexOf("6C")).not.toEqual(-1);
+      // expect(player1.cards.indexOf("9H")).not.toEqual(-1);
+    });
+
+    it("should not have the same deck order after shuffling", function(){
+
+      expect(game.deck.indexOf("AS")).toEqual(0);
+      expect(game.deck.indexOf("AC")).toEqual(26);
+      expect(game.deck.indexOf("KD")).toEqual(51);
+
+      game.shuffleCards();
+
+      console.log(game.deck);
+
+      var ace_of_spades_shuffled = (game.deck.indexOf("AS") !== 0);
+      var ace_of_clubs_shuffled = (game.deck.indexOf("AC") !== 26);
+      var king_of_diamonds_shuffled = (game.deck.indexOf("KD") !== 51);
+
+      var all_cards_shuffled = (ace_of_spades_shuffled && ace_of_clubs_shuffled && king_of_diamonds_shuffled);
+
+      // I know this is a brittle test, but I'm not sure how to make it better
+      expect(all_cards_shuffled).toBe(true);
+    });
+  });
+
+});
